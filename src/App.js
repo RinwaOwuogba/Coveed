de406b0cd2f37e81a67e29d240292c7036bebe2d
bhorlarh <bhorlarinwah@gmail.com>
2020-12-30 20:36:29 +0100

    added doctor prescription screen

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import PatientDetails from './pages/PatientDetails/PatientDetails';
 import DoctorLandingPage from './pages/DoctorLandingPage/DoctorLandingPage';
 import DoctorSignUpPage from './pages/DoctorSignUp/DoctorSignUp';
 import DoctorHome from './pages/DoctorHome/Doctor-home';
+import AddPrescription from './pages/AddPrescription/add-prescription';
 
 import './App.css';
 
@@ -19,10 +20,11 @@ function App() {
 				<Route path='/signup' exact component={Signup} />
 				<Route path='/Eval' exact component={Evaluation} />
 				<Route path='/Patient' exact component={Patient} />
+				<Route path='/Patient-details' exact component={PatientDetails} />
 				<Route path='/doctor/login' exact component={DoctorLandingPage} />
 				<Route path='/doctor/signup' exact component={DoctorSignUpPage} />
 				<Route path='/doctor/home' exact component={DoctorHome} />
-				<Route path='/Patient-details' exact component={PatientDetails} />
+				<Route path='/add-prescription' exact component={AddPrescription} />
 			</Switch>
 		</Router>
 	);
---
+++ src/pages/AddPrescription/add-prescription.js
@@ -0,0 +1,171 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import firstAid from "./../../assets/svg/first-aid.svg";
+import calendar from "./../../assets/svg/calendar.svg";
+
+import "./add-prescription.css";
+
+class AddPrescription extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      medicineName: "",
+      dosage: "",
+      frequency: "",
+      startDate: "",
+      endDate: "",
+      addPrescriptionError: false,
+      addPrescriptionSuccess: false,
+    };
+  }
+
+  onInputChange = (stateKey, event) => {
+    if (this.state.addPrescriptionError) {
+      this.setState({ addPrescriptionError: false });
+    }
+
+    this.setState({ [stateKey]: event.target.value });
+  };
+
+  handlePrescriptionAdd = () => {
+    if (this.state.addPrescriptionError) {
+      this.setState({ addPrescriptionError: false });
+    }
+
+    const timegap =
+      this.state.frequency === "1"
+        ? "days=1"
+        : "hours=" + Math.round(24 / this.state.frequency);
+    const prescriptionInfo = {
+      user_id: this.props.location.user_id,
+      name: this.state.medicineName,
+      info: [
+        [this.state.dosage, this.state.startDate, this.state.endDate],
+        timegap,
+      ],
+    };
+
+    const url = "https://coveedapp.herokuapp.com/doctors/add_prescription";
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        doc_csrf_access_token: this.props.location.doctorAccessToken,
+        "Access-Control-Allow-Origin": "*",
+      },
+      credentials: "include",
+      body: JSON.stringify(prescriptionInfo),
+    };
+
+    fetch(url, requestOptions)
+      .then((response) => {
+        if (!response.ok) {
+          console.log(response);
+          throw new Error("Network response was not ok");
+        }
+
+        this.setState({ addPrescriptionSuccess: true });
+
+        // wait a bit before redirecting the doctor
+        setTimeout(() => {
+          this.props.history.push("/Patient-details");
+        }, 1000);
+      })
+      .catch((error) => {
+        console.error("There has been a problem fetching user data", error);
+        this.setState({ addPrescriptionError: true });
+        return;
+      });
+  };
+
+  render() {
+    return (
+      <div className="add-prescription-page">
+        <h2 className="page-title">Add Prescription</h2>
+
+        {this.state.addPrescriptionError && (
+          <p className="add-perscription_msg add-perscription_error">
+            Sorry, we encountered an error while trying to submit the new
+            prescription. Please try again later.
+          </p>
+        )}
+
+        {this.state.addPrescriptionSuccess && (
+          <p className="add-perscription_msg add-perscription_success">
+            Prescription added successfully
+          </p>
+        )}
+
+        <div className="medicine-name-div">
+          <label htmlFor="name">Medicine Name</label>
+          <img src={firstAid} alt="medicine" />
+          <input
+            onChange={(event) => this.onInputChange("medicineName", event)}
+            value={this.state.medicineName}
+            type="text"
+            id="medicine-name"
+            className="medicine-name"
+            name="medicine-name"
+          ></input>
+        </div>
+
+        <div className="dosage-div">
+          <div className="dosage">
+            <label htmlFor="dosage">Dosage</label>
+            <input
+              onChange={(event) => this.onInputChange("dosage", event)}
+              value={this.state.dosage}
+              min="0"
+              type="number"
+              id="dosage"
+              name="dosage-value"
+            ></input>
+          </div>
+          <div className="frequency">
+            <label htmlFor="frequency">Frequency</label>
+            <div className="input">
+              <input
+                onChange={(event) => this.onInputChange("frequency", event)}
+                value={this.state.frequency}
+                min="0"
+                type="number"
+                id="frequency"
+                name="frequency-value"
+              ></input>
+              <span>/Day</span>
+            </div>
+          </div>
+        </div>
+        <div className="dates">
+          <div className="date-div">
+            <label htmlFor="startDate">Start Date(optional)</label>
+            <img src={calendar} alt="calendar" />
+            <input
+              onChange={(event) => this.onInputChange("startDate", event)}
+              value={this.state.startDate}
+              type="date"
+              id="startDate"
+              name="start-date-value"
+            ></input>
+          </div>
+          <div className="date-div">
+            <label htmlFor="endDate">End Date(optional)</label>
+            <img src={calendar} alt="calendar" />
+            <input
+              onChange={(event) => this.onInputChange("endDate", event)}
+              value={this.state.endDate}
+              type="date"
+              id="endDate"
+              name="end-date-value"
+            ></input>
+          </div>
+        </div>
+        <Link to="/Patient-details" className="btn save-btn">
+          Save
+        </Link>
+      </div>
+    );
+  }
+}
+
+export default AddPrescription;
